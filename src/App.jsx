77944d78470b58695cf7e861782ead528d1226ce
Lada <lada11lada@gmail.com>
2024-08-06 17:30:27 +0200

    saving feedback in LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import css from './App.module.css';
 import Description from './components/Description.jsx';
 import Options from './components/Options.jsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Feedback from './components/Feedback.jsx';
 
 function App() {
-
-  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  const initializeState = () => {
+    const storedFeedback = localStorage.getItem('feedback');
+    return storedFeedback ? JSON.parse(storedFeedback) : { good: 0, neutral: 0, bad: 0 };
+  };
+  const [feedback, setFeedback] = useState(initializeState);
   const hasFeedback = Object.values(feedback).some((value) => value > 0);
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
   return (
         <>
             <div className={css.container}>
